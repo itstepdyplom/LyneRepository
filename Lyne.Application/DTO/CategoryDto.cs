62a75689c88ce674bacb2af98eacfb57331d7f37
Marko <m.savkalviv@gmail.com>
2025-12-04 17:02:30 +0200

    Added id to CategoryDto

--- Lyne.Application/DTO/CategoryDto.cs
+++ Lyne.Application/DTO/CategoryDto.cs
@@ -5,6 +5,7 @@ namespace Lyne.Application.DTO;
 
 public class CategoryDto
 {
+    public Guid Id { get; set; }
     [Required(ErrorMessage = "Name is required!"),MaxLength(50)]
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
